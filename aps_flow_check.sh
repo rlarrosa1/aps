91a35c5e55335be1eff09a480b4e55ec99fa4621
Pablo-UMA <pol93ronda@gmail.com>
2021-02-21 13:38:33 +0100

    aps_flow_check.sh
    Comentarios añadidos a flo_check.sh

--- aps_flow_check.sh
+++ aps_flow_check.sh
@@ -1,8 +1,15 @@
 #!/bin/bash 
 
+# This script will check if the flow has obvious errors within it. The script runs every time
+# aps_create_skelleton.sh is executed
+
 . helper.sh
 
 export l1=`cat $INPUTFILE|tail -n +2`
+
+# The next loop will create the list of possible jobs based on the different columns in the input file
+# The loop reads the file execpt the first line, that is the name of the columns, and ignores the space and tabs
+# simbols.
 
 while read i2 ; do
   export tmp=`echo $i2|tr -s "\t" " "|sed 's/[ \t]*$//'|tr " " "-"`
@@ -10,7 +17,9 @@ while read i2 ; do
   export l2=`echo $l2 $i`
 done < <(cat $INPUTFILE|tail -n +2|tr -s "\t" " "|sed 's/[ \t]*$//' | tr -s " " "-"|grep - )
 
-
+# This function apperas if the user introduces -h as parameter in the script. The functions display
+# the different opstions and a short explanation of the parameters which can be introduced as input
+# in the script.
 
 function show_help
 {
@@ -21,6 +30,8 @@ function show_help
 	echo "-a 		check all scripts in case of error, doesn't stops at the first error of a sample."
 }
 
+# Initialazing some variables needed in the flow ahead.
+
 OPTIND=1
 unset CHECK
 export FILTER_SCRIPTS="[1-9]*.sh"
@@ -29,6 +40,8 @@ export INFO=false
 export VERBOSE=false
 export CHECK_AFTER_ERR=false
 export LOGERR="commands_to_clean_errors.txt"
+
+# This loop will read the input parameters in the screen and it will perform the right acction depending on the parameters
 
 while getopts "h?vd:s:i:a" opt; do
     case "$opt" in
@@ -57,6 +70,8 @@ shift $((OPTIND-1))
 
 echo `date` Begin check >> $LOGERR
 
+# The next function checks if a job with a name passed as parameter already exists.
+
 function work_name
 {
   check=`grep "#SBATCH -J " $1|sed "s/#SBATCH -J //"`
@@ -68,8 +83,13 @@ function work_name
   return 1
 }
 
+# The next variables will allocated the list of scripts and those with unfify behavior.
+
 scripts_list=`cd ${JOB_SOURCE};ls $FILTER_SCRIPTS 2> /dev/null`
 unify_scripts_list=`cd ${JOB_SOURCE};ls [1-9]*${SYNC_JOB}*sh 2> /dev/null`
+
+# If unify list is greater than five then a message will be desplayed in the screen
+# warning to the user that maybe there is a better way to do the flow.
 
 if test `echo $unify_scripts_list |wc -w` -gt 5 ; then 
   echo
@@ -87,7 +107,8 @@ if test `echo $scripts_list |wc -w` -eq 0 ; then
   exit 1
 fi
 
-# chequea tha ouput names, so they are not repeated:
+# It checks the output names and makes sure that there are not duplicated.
+
 export rep_out=`grep -h -- "CH --output=" ${JOB_SOURCE}/*sh|sort |uniq -c|sort -n|tail -1|awk '{print $1}'`
 if test $rep_out != 1 ; then
   echo In the scripts the filenames of the OUTPUT files should be unique, but this one is repeated in several scripts :
@@ -107,6 +128,8 @@ PROCESS_LINE_JOBS=`echo $scripts_list|grep "$PROCESS_LINE"`
 greatest_size_job=5
 greatest_size_job_name=""
 
+# The next loop will go throught the job list and will do some validations as nomenclature.
+
 for job in $scripts_list ; do
   # checks if this script process all the line at the same time
   let index=1
@@ -125,20 +148,20 @@ for job in $scripts_list ; do
 	  echo
 	  $exit 1
   fi
-   if ! test "${job:0:1}" -eq "${JOB_NAME:0:1}" ; then 
-    echo Problem in job $job :
-    echo 
-	  if ! test -z "$JOB_NAME" ; then 
-	  	echo The job name is $JOB_NAME
-	  fi
-	  echo  It should begin with the same number that the job inside the flow.
-	  echo 
-	  echo For example, in this case, inside the script $job
-	  echo  it should say:
-	  echo \#SBATCH -J $job
-	  echo
-	  exit 1
-   fi
+  if ! test "${job:0:1}" -eq "${JOB_NAME:0:1}" ; then 
+   echo Problem in job $job :
+   echo 
+	 if ! test -z "$JOB_NAME" ; then 
+	 	echo The job name is $JOB_NAME
+	 fi
+	 echo  It should begin with the same number that the job inside the flow.
+	 echo 
+	 echo For example, in this case, inside the script $job
+	 echo  it should say:
+	 echo \#SBATCH -J $job
+	 echo
+	 exit 1
+  fi
   if test `echo $JOB_NAME|wc -c` -gt $greatest_size_job && ! echo $JOB_NAME|grep $SYNC_JOB > /dev/null ; then 
     greatest_size_job=`echo $JOB_NAME|wc -c` 
     greatest_size_name=$JOB_NAME
@@ -146,7 +169,7 @@ for job in $scripts_list ; do
 done
 
 
-# checks input file
+# The next validation is for the input file and if it follows a right structure.
 
 merge_lines=`cat $INPUTFILE|tail -n +2|tr -s "\t" " "|sed 's/[ \t]*$//' |tr " " "-" |sort|uniq|wc -l`
 lines=`cat $INPUTFILE|tail -n +2|uniq|wc -l`
