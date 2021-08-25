57f821ad6ec956f93f29be2b5a8e3f1d23a955d3
Pablo-UMA <pol93ronda@gmail.com>
2021-08-25 18:49:59 +0200

    Últimos ficheros comentados y sangrados

--- aps_delete_steps_output_for_replay.sh
+++ aps_delete_steps_output_for_replay.sh
@@ -1,4 +1,5 @@
-
+# The goal of this script is delete the output files of executed jobs in order to
+# replay the experiment
 
 . helper.sh
 
@@ -19,10 +20,7 @@ function show_help
 OPTIND=1
 unset CHECK
 export FILTER_SCRIPTS="[1-9]*.sh"
-export INFOFILE=information.out
-export INFO=false
 export VERBOSE=false
-export CHECK_AFTER_ERR=false
 export DELETE_FILES="delete_files_to_replay.sh"
 
 let nerrors=0
@@ -64,38 +62,28 @@ rm $CACHE_SQUEUE_FILE 2> /dev/null
 for i in $l1 $l2 $l3 ; do
   pushd . > /dev/null
   if ! cd $i 2> /dev/null ; then
-    # no existe el directorio
     continue
   fi
   lista_scripts=`ls $FILTER_SCRIPTS 2>/dev/null`
   for job in $lista_scripts ; do
     let nchecks=${nchecks}+1
-#echo to test en $i el job $job
     if grep \#SBATCH $job > /dev/null ; then
-# primero mira si ya existe en el sistema de colas
-      #if existe_trabajo $i $job > /dev/null 2>&1 ; then
-      if existe_trabajo $i $job ; then
-        echo `date` job $job from sample $i is in the queue system, with id: $ADD_DEPEND.
-# si no existe mira si ya ha terminado correctamente
+      if work_exists $i $job ; then
+        echo `date` Job $job from sample $i is in the queue system, with id: $ADD_DEPEND.
       else
-        if trabajo_terminado_correctamente $i $job ; then
+        if work_done_successfully $i $job ; then
           if test $VERBOSE == "true" ; then
-            echo `date` job $job from sample $i has finished correctly
+            echo `date` Job $job from sample $i has finished correctly
           fi
-#  if ! trabajo_con_fichero_salida $i $job > /dev/null ; then
           if ! trabajo_con_fichero_salida $i $job ; then
                 echo `date` job $job from sample $i never has been run, no output file exists.
                 let unexec=${unexec}+1
           else
             echo rm ${i}/${OUTPUT_FILE} >> ../${DELETE_FILES}
             echo rm ${i}/${OUTPUT_FILE}|sed s/.out/.err/ >> ../${DELETE_FILES}
-  	    grep CHECK $job|sed s/\#CHECK// | sed s\#ls\ -l\#\# |sed s\#\|\#\# >> ../${DELETE_FILES}
+  	        grep CHECK $job|sed s/\#CHECK// | sed s\#ls\ -l\#\# |sed s\#\|\#\# >> ../${DELETE_FILES}
             echo `date` job $job from sample $i has finished with errors, outputfile: $OUTPUT_FILE
             let nerrors=${nerrors}+1
-#           if test $CHECK_AFTER_ERR != "true" ; then 
-              # continues in the next sample
- #             break
-#           fi
           fi
         fi
       fi
